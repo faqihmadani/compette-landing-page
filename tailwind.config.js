1c85cc7bcc54be3080aed715174ec34c8a2e6411
faqihmadani <faqihfaqih22@gmail.com>
2021-01-26 17:30:21 +0700

    selesai membuat navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily:{
+      'Asap':'Asap'
+    },
+    colors:{
+      'myGreen':'#10495C',
+      'myYellow':'#F2AD5F',
+      'myGold':'#986121',
+      'myGray':'#B0C3C9',
+      'myBlack':'#0F1D37',
+      'white':'#FFFFFF'
+    },
     extend: {},
   },
   variants: {
